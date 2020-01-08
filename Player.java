2f8ae5f635514ff86d1b80df14209f006210c199
Pluficorn <>
2020-01-08 13:59:53 +0100

    Created Class player

---
+++ Player.java
@@ -0,0 +1,29 @@
+
+/**
+ * Class Player -- all the information about the player, like a coin pouch and the inventory
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ * 
+ * The inventory carries all the items of the player, which is determined by a maxiumum weight.
+ * The coin pouch carries all the coins, determined by count.
+ * 
+ * @author N.Verkade
+ * @version 08-01-2019
+ */
+
+public class Player
+{
+    // instance variables - vervang deze door jouw variabelen
+    
+    
+    /**
+     * Constructor voor objects van class Player
+     */
+    public Player()
+    {
+        // geef de instance variables een beginwaarde
+        x = 0;
+    }
+
+}
