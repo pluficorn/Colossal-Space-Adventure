2513174d202e111d3a6521bbec77a29d82293ec7
Esther <estherzr97@gmail.com>
2020-01-15 13:01:07 +0100

    Update Player.java
    indentions

--- Player.java
+++ Player.java
@@ -16,127 +16,127 @@ import java.lang.Math;
  */
 
 public class Player {
-	private ArrayList<Item> inventory;
-	private int maxWeight;
-	private Room currentRoom;
+    private ArrayList<Item> inventory;
+    private int maxWeight;
+    private Room currentRoom;
 
-	/**
-	 * Constructor voor objects van class Player
-	 */
-	public Player(int maxWeight, Room currentRoom) {
-		this.maxWeight = maxWeight;
-		this.currentRoom = currentRoom;
-		this.inventory = new ArrayList<>();
-		// pickupItem(new Item("Handbook"); fixthis
-	}
+    /**
+     * Constructor voor objects van class Player
+     */
+    public Player(int maxWeight, Room currentRoom) {
+        this.maxWeight = maxWeight;
+        this.currentRoom = currentRoom;
+        this.inventory = new ArrayList<>();
+        // pickupItem(new Item("Handbook"); fixthis
+    }
 
-	/**
-	 * Move player to specified room
-	 */
-	public void setRoom(Room room) {
-		if (room.isTrapdoor()) {
-			this.currentRoom = room;
-			goTrapdoor();
-		} else {
-			this.currentRoom = room;
-		}
-	}
+    /**
+     * Move player to specified room
+     */
+    public void setRoom(Room room) {
+        if (room.isTrapdoor()) {
+            this.currentRoom = room;
+            goTrapdoor();
+        } else {
+            this.currentRoom = room;
+        }
+    }
 
-	/**
-	 * Teleport player to random room, saved in the randomRooms ArrayList.
-	 */
-	public void goTrapdoor() {
-		// Get ArrayList of trapdoors
-		ArrayList<Room> trapdoorLocations = getRoom().getTrapdoorLocations();
+    /**
+     * Teleport player to random room, saved in the randomRooms ArrayList.
+     */
+    public void goTrapdoor() {
+        // Get ArrayList of trapdoors
+        ArrayList<Room> trapdoorLocations = getRoom().getTrapdoorLocations();
 
-		// Check if that ArrayList contains at least one item
-		if (trapdoorLocations.size() > 0) {
-			int rand = (int) (Math.random() * getRoom().getTrapdoorLocations().size());
-			Room randomRoom = trapdoorLocations.get(rand);
-			System.out.println("Wow, you tripped and fell in a hole! You are all turned around.");
+        // Check if that ArrayList contains at least one item
+        if (trapdoorLocations.size() > 0) {
+            int rand = (int) (Math.random() * getRoom().getTrapdoorLocations().size());
+            Room randomRoom = trapdoorLocations.get(rand);
+            System.out.println("Wow, you tripped and fell in a hole! You are all turned around.");
 
-			// Teleport player to room and clear players history
-			setRoom(randomRoom);
-			// history.clear();
-		} else {
-			System.out.println(
-					"Well, the hole ends up leading nowhere. Awkward. Maybe the game developers should add some trapdoor locations...");
-		}
-	}
+            // Teleport player to room and clear players history
+            setRoom(randomRoom);
+            // history.clear();
+        } else {
+            System.out.println(
+                "Well, the hole ends up leading nowhere. Awkward. Maybe the game developers should add some trapdoor locations...");
+        }
+    }
 
-	/**
-	 * Get the room the player is currently in.
-	 * 
-	 * @return current room
-	 */
-	public Room getRoom() {
-		return this.currentRoom;
-	}
+    /**
+     * Get the room the player is currently in.
+     * 
+     * @return current room
+     */
+    public Room getRoom() {
+        return this.currentRoom;
+    }
 
-	/**
-	 * Method that drops a chosen item by the player
-	 */
-	public void dropItem(Item item) {
-		inventory.remove(item);
-	}
+    /**
+     * Method that drops a chosen item by the player
+     */
+    public void dropItem(Item item) {
+        inventory.remove(item);
+    }
 
-	/**
-	 * return the item from inventory based on index
-	 */
-	public Item getInventoryItemFromString(String itemName) {
-		for (Item item : inventory) {
-			if (item.getName() != null && item.getName().equals(itemName)) {
-				return item;
-			}
-		}
+    /**
+     * return the item from inventory based on index
+     */
+    public Item getInventoryItemFromString(String itemName) {
+        for (Item item : inventory) {
+            if (item.getName() != null && item.getName().equals(itemName)) {
+                return item;
+            }
+        }
 
-		// If no item was found, return null
-		return null;
-	}
+        // If no item was found, return null
+        return null;
+    }
 
-	/**
-	 * Method that picks up a chosen item by the player
-	 */
-	public void pickUpItem(Item item) {
-		// Check if is already in inventory
-		if (inventory.contains(item)) {
-			int itemIndex = inventory.indexOf(item);
-			// increase the count of an item with
-			inventory.get(itemIndex).setCount((inventory.get(itemIndex).getCount()) + item.getCount());
-		}
-		// If in inventory, increase amount and wheight of item
-		inventory.add(item);
-	}
+    /**
+     * Method that picks up a chosen item by the player
+     */
+    public void pickUpItem(Item item) {
+        // Check if is already in inventory
+        if (inventory.contains(item)) {
+            int itemIndex = inventory.indexOf(item);
+            // increase the count of an item with
+            inventory.get(itemIndex).setCount((inventory.get(itemIndex).getCount()) + item.getCount());
+        }
+        // If in inventory, increase amount and wheight of item
+        inventory.add(item);
+    }
 
-	/**
-	 * Set maximum weight the player can carry in grams.
-	 */
-	public void setMaxWeight(int weight) {
-		this.maxWeight = weight;
-	}
+    /**
+     * Set maximum weight the player can carry in grams.
+     */
+    public void setMaxWeight(int weight) {
+        this.maxWeight = weight;
+    }
 
-	/**
-	 * Get the maximum weight the player can carry in grams.
-	 */
-	public int getMaxWeight() {
-		return maxWeight;
-	}
+    /**
+     * Get the maximum weight the player can carry in grams.
+     */
+    public int getMaxWeight() {
+        return maxWeight;
+    }
 
-	/**
-	 * Get the total weight the player is carrying in grams.
-	 */
-	public int calculateTotalWeight() {
-		int totalWeight = 0;
-		for (Item item : inventory) {
-			totalWeight += (item.getWeight() * item.getCount());
-		}
-		return totalWeight;
-	}
+    /**
+     * Get the total weight the player is carrying in grams.
+     */
+    public int calculateTotalWeight() {
+        int totalWeight = 0;
+        for (Item item : inventory) {
+            totalWeight += (item.getWeight() * item.getCount());
+        }
+        return totalWeight;
+    }
 
-	/**
-	 * Get the inventory of the player
-	 */
-	public ArrayList<Item> getInventory() {
-		return inventory;
-	}
+    /**
+     * Get the inventory of the player
+     */
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
 }
