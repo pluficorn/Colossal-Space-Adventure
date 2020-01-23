28480023ff29d7ce8a0a0f1a696a3d605e202e90
Esther <estherzr97@gmail.com>
2020-01-23 14:06:22 +0100

    fixed small bugs. README updatet
    Tekst voor README geüpdatet.
    added text for interpreter leaving room.
    fixed outofbounds error voor trapdoor

--- Player.java
+++ Player.java
@@ -140,8 +140,8 @@ public class Player {
 
         // Check if that ArrayList contains at least one item
         if (trapdoorLocations.size() > 0) {
-            // Generate a random index for which room to pick
-            int rand = (int) (Math.round(Math.random() * getRoom().getTrapdoorLocations().size()));
+            // Generate a random index for which room to pick. -1 because size is always 1 more than the highest index value
+            int rand = (int) (Math.round(Math.random() * getRoom().getTrapdoorLocations().size()-1));
             
             // Save this room
             Room randomRoom = trapdoorLocations.get(rand);
